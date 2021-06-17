76bbcb0cf61dda965a88eddb1c130e68663382f4
Lucas Nieuwenhout <lucasnwnht@gmail.com>
2021-06-16 20:37:55 -0600

    Updates to ImageParser

--- ImageParser.R
+++ ImageParser.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(imager)
 
 # Load in the image
-im <- load.image("./ReferenceImages/CanadianFlag.png")
+im <- load.image("./ReferenceImages/rainbowApple.png")
 
 plot(im)
 
@@ -23,23 +23,24 @@ ywidth <- round(xwidth * imageRatio)
 
 
 # Create sample sequence
-xsample <- round(seq(0, max(imdf$x), length.out = xwidth))
-ysample <- round(seq(0, max(imdf$y), length.out = ywidth))
+xsample <- round(seq(1, max(imdf$x), length.out = xwidth))
+ysample <- round(seq(1, max(imdf$y), length.out = ywidth))
 
 # Create iamge dataframe
-imageMap <- data.frame(x = rep(1:xwidth, times = ywidth), y = rep(1:ywidth, each = xwidth))
+imageMap <- data.frame(x = rep(1:xwidth, times = ywidth), y = rep(1:ywidth, each = xwidth), R = 0, G = 0, B = 0)
 
 
-
-
+# Parse through
 for (i in 1:xwidth) {
   for (j in 1:ywidth) {
-    
+    imageMap$R[imageMap$x == i & imageMap$y == j] <- imdf$R[imdf$x == xsample[i] & imdf$y == ysample[j]]
+    imageMap$G[imageMap$x == i & imageMap$y == j] <- imdf$G[imdf$x == xsample[i] & imdf$y == ysample[j]]
+    imageMap$B[imageMap$x == i & imageMap$y == j] <- imdf$B[imdf$x == xsample[i] & imdf$y == ysample[j]]
   }
 }
 
-
-
+# Plot the image with real colour using the rgb() and scale_color_identity() function
+imageMap %>% ggplot(aes(x, -y, col = rgb(R, G, B))) + geom_point(size = 4) + scale_color_identity()
 
 
 
